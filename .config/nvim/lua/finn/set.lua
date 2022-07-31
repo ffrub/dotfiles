e536ebb2dc9f181ea9edbfd182509e3563188fc8
ffrub <ff.rueb@gmail.com>
2022-07-31 14:35:01 +0200

    build(zshrc): add general vim config

--- .config/nvim/lua/finn/set.lua
+++ .config/nvim/lua/finn/set.lua
@@ -1,15 +1,26 @@
 vim.opt.nu = true
 vim.opt.relativenumber = true
+vim.opt.scrolloff = 8
+
+vim.opt.smartindent = true
+vim.opt.wrap = false
+
+vim.opt.colorcolumn = '100'
 
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
+
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
 
-vim.opt.smartindent = true
+vim.opt.hidden = true
+vim.opt.errorbells = false
 
-vim.opt.wrap = false
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = '~/.vim/undodir'
+vim.opt.undofile = true
 
 vim.g.mapleader = " "
