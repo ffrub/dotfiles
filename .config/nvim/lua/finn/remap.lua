e67ad775b856808b74c8326d1d0f861fedbfe01c
ffrub <ff.rueb@gmail.com>
2022-07-31 14:43:15 +0200

    build(vim): add remaps for window nav

--- .config/nvim/lua/finn/remap.lua
+++ .config/nvim/lua/finn/remap.lua
@@ -11,5 +11,18 @@ nnoremap("<leader>fh", "<cmd>Telescope help_tags<CR>")
 -- open netrw
 nnoremap("<leader>pv", "<cmd>Ex<CR>")
 
+-- jump to pane
+nnoremap("<C-j>", "<C-w>j")
+nnoremap("<C-k>", "<C-w>k")
+nnoremap("<C-h>", "<C-w>h")
+nnoremap("<C-l>", "<C-w>l")
+
+-- move pane
+nnoremap("<C-w>j", "<C-w>J")
+nnoremap("<C-w>k", "<C-w>K")
+nnoremap("<C-w>h", "<C-w>H")
+nnoremap("<C-w>l", "<C-w>L")
+
+
 inoremap("jh", "<Esc>")
 
