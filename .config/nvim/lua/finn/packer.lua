ca46f6ef7689b43e7bbe51a5069a3ae8b212532b
ffrub <ff.rueb@gmail.com>
2022-10-02 14:30:04 +0200

    fix(vim): remove random word from packer.lua

--- .config/nvim/lua/finn/packer.lua
+++ .config/nvim/lua/finn/packer.lua
@@ -1,4 +1,4 @@
-auto-- This file can be loaded by calling `lua require('plugins')` from your init.vim
+-- This file can be loaded by calling `lua require('plugins')` from your init.vim
 
 -- Only required if you have packer configured as `opt`
 vim.cmd [[packadd packer.nvim]]
